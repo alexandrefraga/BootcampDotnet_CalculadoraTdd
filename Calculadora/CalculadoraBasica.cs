a56275d0f90ab731cb128b9c5c3d75b2ebf7d759
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2023-10-12 14:17:37 -0300

    Solução e projetos criados

---
+++ Calculadora/CalculadoraBasica.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class CalculadoraBasica
+    {
+        public int Soma(int a, int b)
+        {
+            return 0;
+        }
+
+        public int Subtrair(int a, int b)
+        {
+            return 0;
+        }
+
+        public int Multiplicar(int a, int b)
+        {
+            return 0;
+        }
+
+        public int Dividir(int a, int b)
+        {
+            return 0;
+        }
+
+        public List<int> Historico()
+        {
+            return new List<int>();
+        }
+    }
+}
---
+++ Testes/CalcuradoraTest.cs
@@ -0,0 +1,14 @@
+
+
+using Calculadora;
+
+namespace Testes;
+
+public class CalculadoraTest
+{
+    [Fact]
+    public void Test1()
+    {
+        CalculadoraBasica calc = new CalculadoraBasica();
+    }
+}
