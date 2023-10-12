61017f72426f44db47ecacff303c2c1e56f1b597
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2023-10-12 14:47:11 -0300

    Test: Deve retornar o valor correto da subtração

--- Testes/CalcuradoraTest.cs
+++ Testes/CalcuradoraTest.cs
@@ -17,4 +17,16 @@ public class CalculadoraTest
 
         Assert.Equal(resultadoEsperado, resultado);
     }
+
+    [Theory]
+    [InlineData(8, 2, 6)]
+    [InlineData(15, 4, 11)]
+    public void DeveRetornarASubtracaoCorreta(int n1, int n2, int resultadoEsperado)
+    {
+        CalculadoraBasica calc = new CalculadoraBasica();
+
+        var resultado = calc.Subtrair(n1, n2);
+
+        Assert.Equal(resultadoEsperado, resultado);
+    }
 }
