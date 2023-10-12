296eb13d6958a975a88987361c809508de6bd2d1
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2023-10-12 16:15:07 -0300

    feat: Adiciona resultados ao histórico

--- Calculadora/CalculadoraBasica.cs
+++ Calculadora/CalculadoraBasica.cs
@@ -5,31 +5,49 @@ using System.Threading.Tasks;
 
 namespace Calculadora
 {
+
     public class CalculadoraBasica
     {
+        private List<string> _historico;
+        public CalculadoraBasica() => _historico = new List<string>();
+
+        private void AddHistorico(string texto)
+        {
+            _historico.Add(texto);
+            _historico.RemoveRange(0, _historico.Count > 3 ? _historico.Count - 3 : 0);
+        }
+
         public int Soma(int a, int b)
         {
-            return a + b;
+            var result = a + b;
+            AddHistorico($"{a} + {b} = {result}");
+            return result;
         }
 
         public int Subtrair(int a, int b)
         {
-            return a - b;
+            var result = a - b;
+            AddHistorico($"{a} - {b} = {result}");
+            return result;
         }
 
         public int Multiplicar(int a, int b)
         {
-            return a * b;
+            var result = a * b;
+            AddHistorico($"{a} * {b} = {result}");
+            return result;
         }
 
         public int Dividir(int a, int b)
         {
-            return a / b;
+            var result = a / b;
+            AddHistorico($"{a} / {b} = {result}");
+            return result;
         }
 
         public List<string> Historico()
         {
-            return new List<string>();
+            return _historico;
         }
     }
 }
