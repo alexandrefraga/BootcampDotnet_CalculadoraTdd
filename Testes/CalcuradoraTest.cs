53d49ec4d4386055799f0adcc3f612a6e3841d37
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2023-10-12 14:42:10 -0300

    Test: Deve retornar a soma correta

--- Testes/CalcuradoraTest.cs
+++ Testes/CalcuradoraTest.cs
@@ -6,9 +6,15 @@ namespace Testes;
 
 public class CalculadoraTest
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(1, 2, 3)]
+    [InlineData(5, 4, 9)]
+    public void DeveRetornarASomaCorreta(int n1, int n2, int resultadoEsperado)
     {
         CalculadoraBasica calc = new CalculadoraBasica();
+
+        var resultado = calc.Soma(n1, n2);
+
+        Assert.Equal(resultadoEsperado, resultado);
     }
 }
